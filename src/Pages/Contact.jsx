f0142f3af28f2d1f8be4058cd14d489c5138e94b
YepaProg <paumedvazquez@gmail.com>
2025-05-09 20:17:32 +0200

    Fix contact

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -13,7 +13,7 @@ export const Contact = () => {
                     <img src={ContactImg} alt="A person holding a phone" className="w-full rounded-2xl object-cover h-[930px]" />
                 </div>
                 <div className="w-full lg:w-[49%] h-full flex flex-col items-center justify-center gap-5 md:gap-10">
-                    <a href="https://wa.me/34653853861" className="flex flex-col items-center justify-center bg-[#171733] rounded-2xl h-60 w-full border-[#8D4ACD] border-3 hover:bg-[#3A2661] transition duration-300">
+                    <a href="https://wa.me/34653853861" target="_blank" className="flex flex-col items-center justify-center bg-[#171733] rounded-2xl h-60 w-full border-[#8D4ACD] border-3 hover:bg-[#3A2661] transition duration-300">
                         <img src={Icon} alt="Nuevotech icon" className="w-20 h-20" />
                         <p className="text-[#aaaac1]">Chat y soporte</p>
                         <h3 className="text-[#E5F6FF] text-4xl font-semibold">+34 653 85 38 61</h3>
