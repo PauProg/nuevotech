487a75bfdc223788c34a763e3830e5484379c20f
YepaProg <paumedvazquez@gmail.com>
2025-05-15 18:53:53 +0200

    Fixed again the responsive

--- src/Components/Sections/AboutHome.jsx
+++ src/Components/Sections/AboutHome.jsx
@@ -10,7 +10,7 @@ export const AboutHome = () => {
             <h2 className="h2 text-center text-3xl md:text-4xl lg:text-5xl text-[#E5F6FF] font-semibold">¿Porqué escogernos a nosotros?</h2>
             <RevealOnScroll>
                 <div className="bloqueInfo w-full flex flex-col gap-5 lg:gap-0 justify-center items-center lg:flex-row">
-                    <div className="texto flex flex-col items-center justify-center w-full lg:w-1/2 gap-3">
+                    <div className="texto flex flex-col items-center justify-center w-full lg:w-1/2 gap-6">
                         <h3 className="text-center lg:text-left text-3xl md:text-4xl lg:text-6xl font-semibold text-[#E5F6FF]">Equipo multidisciplinar con experiencia real</h3>
                         <p className="text-center lg:text-left font-medium text-lg md:text-xl lg:text-2xl">Contamos con profesionales especializados en redes, sistemas, ciberseguridad, desarrollo web y derecho digital. Esto nos permite ofrecerte una solución completa desde una sola empresa.</p>
                     </div>
@@ -20,7 +20,7 @@ export const AboutHome = () => {
             <RevealOnScroll>
                 <div className="bloqueInfo w-full flex flex-col-reverse gap-5 lg:gap-0 justify-center items-center lg:flex-row">
                     <img src={Solucion} alt="Equipo multidisciplinar con experiencia real" className="w-1/2 object-cover" />
-                    <div className="texto flex flex-col items-center justify-center w-full lg:w-1/2 gap-3">
+                    <div className="texto flex flex-col items-center justify-center w-full lg:w-1/2 gap-6">
                         <h3 className="text-center lg:text-left text-3xl md:text-4xl lg:text-6xl font-semibold text-[#E5F6FF]">Soluciones 100% personalizadas</h3>
                         <p className="text-center lg:text-left font-medium text-lg md:text-xl lg:text-2xl">Cada negocio es distinto. Por eso, analizamos tus necesidades concretas y diseñamos un plan tecnológico a medida para maximizar tu productividad.</p>
                     </div>
@@ -28,7 +28,7 @@ export const AboutHome = () => {
             </RevealOnScroll>
             <RevealOnScroll>
                 <div className="bloqueInfo w-full flex flex-col gap-5 lg:gap-0 justify-center items-center lg:flex-row">
-                    <div className="texto flex flex-col items-center justify-center w-full lg:w-1/2 gap-3">
+                    <div className="texto flex flex-col items-center justify-center w-full lg:w-1/2 gap-6">
                         <h3 className="text-center lg:text-left text-3xl md:text-4xl lg:text-6xl font-semibold text-[#E5F6FF]">Atención cercana y sin tecnicismos</h3>
                         <p className="text-center lg:text-left font-medium text-lg md:text-xl lg:text-2xl">Te explicamos todo de forma clara, sin lenguaje complicado, y te acompañamos en todo el proceso con soporte directo y resolutivo.</p>
                     </div>
@@ -38,7 +38,7 @@ export const AboutHome = () => {
             <RevealOnScroll>
                 <div className="bloqueInfo w-full flex flex-col-reverse gap-5 lg:gap-0 justify-center items-center lg:flex-row">
                     <img src={Tecnologia} alt="Equipo multidisciplinar con experiencia real" className="w-1/2 object-cover" />
-                    <div className="texto flex flex-col items-center justify-center w-full lg:w-1/2 gap-3">
+                    <div className="texto flex flex-col items-center justify-center w-full lg:w-1/2 gap-6">
                         <h3 className="text-center lg:text-left text-3xl md:text-4xl lg:text-6xl font-semibold text-[#E5F6FF]">Tecnología con garantía de futuro</h3>
                         <p className="text-center lg:text-left font-medium text-lg md:text-xl lg:text-2xl">Nos mantenemos actualizados con las últimas tendencias y normativas para ofrecerte siempre herramientas actuales, seguras y escalables.</p>
                     </div>
